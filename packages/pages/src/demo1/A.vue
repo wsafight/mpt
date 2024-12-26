049502b938c0f8003225418fefff52e24610815b
jump-and-jump <984292420@qq.com>
2024-12-27 02:39:29 +0800

    feat: start sidebar

--- packages/pages/src/demo1/A.vue
+++ packages/pages/src/demo1/A.vue
@@ -11,24 +11,5 @@ export default {};
   </template>
   
   <style scoped>
-  .content {
-    display: flex;
-    min-height: 100vh;
-    line-height: 1.1;
-    text-align: center;
-    flex-direction: column;
-    justify-content: center;
-  }
-  
-  .content h1 {
-    font-size: 3.6rem;
-    font-weight: 700;
-  }
-  
-  .content p {
-    font-size: 1.2rem;
-    font-weight: 400;
-    opacity: 0.5;
-  }
   </style>
   
