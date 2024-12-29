a6b24127eb7700b2749ab134bc6f66268c708c51
jump-and-jump <984292420@qq.com>
2024-12-29 19:28:47 +0800

    feat: add mock

---
+++ packages/mock/rslib.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from '@rslib/core';
+
+const isProduction = process.env.ENV === 'production';
+
+export default defineConfig({
+  lib: [
+    {
+      format: isProduction ? 'umd' : 'esm',
+      umdName: 'mptBlocks',
+      syntax: 'es2021',
+      dts: true,
+    },
+  ],
+  output: {
+    filenameHash: !isProduction,
+    filename: {
+      js: isProduction ? 'block.[hash].js' : '[name].js',
+      css: isProduction ? 'block.[hash].css' : '[name].css',
+    },
+    target: 'web',
+    externals: {},
+    minify: isProduction,
+  },
+});
