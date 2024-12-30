86ce2b3741c77b364cddab7bdccec3c0287b9b76
jump-and-jump <984292420@qq.com>
2024-12-30 12:21:23 +0800

    feat: chang code

---
+++ packages/blocks/src/scripts/loadAssets.ts
@@ -0,0 +1,66 @@
+import loadjs from "loadjs";
+
+export interface JsLib {
+  url: string;
+  wrapper: () => any;
+}
+
+const dynamicJsLibs: Record<string, JsLib> = {
+  echarts: {
+    url: "//unpkg.com/echarts@5.6.0/dist/echarts.min.js",
+    wrapper() {
+      return (window as any).echarts;
+    },
+  },
+};
+
+// todo 加载中未处理
+export const loadAssets = (assets: string | string[]) => {
+  let inputIsArray = true;
+  if (typeof assets === "string") {
+    assets = [assets];
+    inputIsArray = false;
+  }
+  const notFound = assets.find((x) => !dynamicJsLibs[x]);
+  if (notFound) {
+    throw new Error("Cannot found asset [" + notFound + "]");
+  }
+  const items = assets.map((name: string) => {
+    const current = dynamicJsLibs[name];
+    const result = current.wrapper();
+    const isSuccess = !!result;
+    return {
+      name,
+      isSuccess,
+      result,
+      ...(!isSuccess && { wrapper: current.wrapper }),
+    };
+  });
+
+  // 需要加载的项
+  const loadingItems = items.filter((x) => !x.isSuccess);
+
+  // 都已经加载的场合
+  if (!loadingItems.length) {
+    return Promise.resolve(
+      inputIsArray ? items.map((x) => x.result) : items[0].result
+    );
+  }
+
+  return new Promise((resolve, reject) => {
+    const urlsToLoad: string[] = loadingItems.map(
+      (item) => dynamicJsLibs[item.name].url
+    ) as string[];
+    loadjs(urlsToLoad, {
+      success: () => {
+        const result = inputIsArray
+          ? items.map((x) => x.wrapper!())
+          : items[0].wrapper!();
+        resolve(result);
+      },
+      error: function (urls) {
+        reject(new Error("loadjs Error"));
+      },
+    });
+  });
+};
